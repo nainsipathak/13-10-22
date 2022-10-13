dacde20a5802513cf625a113611257cea046695c
nainsipathak <112616933+nainsipathak@users.noreply.github.com>
2022-10-13 13:22:24 +0530

    Add files via upload

---
+++ ReverseStr.java
@@ -0,0 +1,34 @@
+package com.test.bll;
+
+import java.util.Scanner;
+
+public class ReverseStr {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string : ");
+		String str = sc.nextLine();
+				
+		char charArray[] =reverseString(str);
+		for(char c:charArray) {
+		System.out.print(c);
+				
+		sc.close();
+		}
+	}
+
+	public static char[] reverseString(String str) {
+		
+		char charArray[] =new char[str.length()];
+		int index=0;
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			charArray[index] = (str.charAt(i));
+			index++;
+			}
+		return charArray;
+
+	}
+
+}
