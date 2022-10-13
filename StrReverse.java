7e3ff79b266af17b98d09cfe24893a6b9ded6a46
nainsipathak <112616933+nainsipathak@users.noreply.github.com>
2022-10-13 13:07:51 +0530

    Add files via upload

--- StrReverse.java
+++ StrReverse.java
@@ -1,23 +1,29 @@
 package com.test.bll;
+import java.util.Scanner;
 
 public class StrReverse {
 	
-	 public static void main(String[] args)
+	 public static void main(String[] args) {
 
-	    {
-
-	        String str = "Nainsi";
-
-	        // conversion from String object to StringBuffer
-
-	        StringBuffer sbfr = new StringBuffer(str);
-
-	        // To reverse the string
-
-	        sbfr.reverse();
-
-	        System.out.println(sbfr);
-
-	    }
+	    Scanner sc = new Scanner(System.in);
+				
+			System.out.println("Enter a string : ");
+			String str = sc.nextLine();
+			System.out.println("Origional string : "+str);
+				
+			reverseString(str);
+				
+			
+		  sc.close();
+			}
+		
+	 public static void reverseString(String str) {
+		
+		 for(int i=str.length()-1; i>=0; i--) {
+			if(i==str.length()-1)
+				System.out.print("Reverse string : ");
+				System.out.print(str.charAt(i));
+				}
+			}
 
 }
